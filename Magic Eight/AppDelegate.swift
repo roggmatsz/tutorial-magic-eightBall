8795167944cca8c787b6f28b327014e1aac04fb2
Rogger Matamoros <roggmatz@gmail.com>
2016-05-29 14:18:20 -0400

    Initial Commit

---
+++ Magic Eight/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Magic Eight
+//
+//  Created by Rogger Matamoros on 5/29/16.
+//  Copyright © 2016 Roggmatz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Magic Eight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Magic Eight
+//
+//  Created by Rogger Matamoros on 5/29/16.
+//  Copyright © 2016 Roggmatz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
